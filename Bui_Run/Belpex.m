1d4ae45712317c12d6f2bca91d759bbe005e4d78
jorengallego <joren.gallego@student.kuleuven.be>
2019-02-26 17:44:35 +0100

    Functie voor binnenhalen belpexprijzen

---
+++ Bui_Run/Belpex.m
@@ -0,0 +1,7 @@
+Belpex2018 = xlsread('BelpexFilter.xlsx');
+BelpexPrice = [];
+for i = 1:8760
+        BelpexPrice = [BelpexPrice, Belpex2018(i), Belpex2018(i), Belpex2018(i), Belpex2018(i)];
+end
+BelpexPrice = BelpexPrice';
+    
